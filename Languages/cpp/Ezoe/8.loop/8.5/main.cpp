715f9b50d763319d356e939e8bb5a8b18c95db24
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-03-25 18:04:25 +0900

    Languages::cpp: recursive #34

---
+++ Languages/cpp/Ezoe/8.loop/8.5/main.cpp
@@ -0,0 +1,33 @@
+#include "../../all.hpp"
+
+int solve(int n) {
+  if (n <= 1)
+    return n;
+  else
+    return n % 10 + 2 * solve(n / 10);
+}
+
+int convert(int n) {
+  if (n > 0)
+    return solve(n);
+  else
+    return -solve(-n);
+}
+
+void output(int binary) {
+  std::cout << binary << "\n"s;
+}
+
+int input() {
+  int x{};
+  std::cin >> x;
+  return x;
+}
+
+int main() {
+  while (true) {
+    auto decimal = input();
+    auto binary = convert(decimal);
+    output(binary);
+  }
+}
