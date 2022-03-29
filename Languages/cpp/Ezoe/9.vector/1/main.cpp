ac54e4d05889ab0792b872a48cf2491025ccd2ea
Lootmann <ryota.tsguawa.sites.tabs@gmail.com>
2022-03-29 21:53:23 +0900

    Languages::cpp - vector

---
+++ Languages/cpp/Ezoe/9.vector/1/main.cpp
@@ -0,0 +1,26 @@
+#include "../../all.hpp"
+
+int main() {
+  std::vector<int> v{8, 3, 7, 4, 2, 9, 3};
+  std::size_t size = v.size();
+
+  for (std::size_t head = 0; head != size; ++head) {
+    std::size_t min = head;
+
+    for (std::size_t i = head + 1; i != size; ++i) {
+      if (v.at(i) < v.at(min)) {
+        min = i;
+      }
+    }
+
+    auto temp = v.at(head);
+    v.at(head) = v.at(min);
+    v.at(min) = temp;
+  }
+
+  // index
+  for (auto num : v) {
+    std::cout << num << " "s;
+  }
+  std::cout << "\n"s;
+}
