0183c68c5ba80d4c72033bd9e1e3d378723e6e6d
Lootmann <ryota.tsguawa.sites.tabs@gmail.com>
2022-03-31 20:37:26 +0900

    Languages::cpp - int #34

--- Languages/cpp/Ezoe/11.int/11.1/main.cpp
+++ Languages/cpp/Ezoe/11.int/11.1/main.cpp
@@ -18,4 +18,26 @@ int main() {
   print(binary);
   print(hex);
   print(hex_c);
+
+  int x = 123;
+  int y = 123u;
+  print(x);
+  print(y);
+
+  auto long_num = 100'0000'0000;
+  print(long_num);
+
+  print(sizeof(std::size_t));
+  print(sizeof(long));
+  print(sizeof(long long));
+  print(sizeof(unsigned long long));
+
+  print(std::numeric_limits<int>::max());
+  print(std::numeric_limits<int>::min());
+
+  print(std::numeric_limits<int>::max());
+  print(std::numeric_limits<int>::min());
+
+  print(std::numeric_limits<unsigned long long>::max());
+  print(std::numeric_limits<unsigned long long>::min());
 }
