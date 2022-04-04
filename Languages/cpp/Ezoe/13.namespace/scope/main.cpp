674a6578c56645df605d4df813dba798ed618ef4
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-04-04 23:04:46 +0900

    Languages::cpp - scope #34

---
+++ Languages/cpp/Ezoe/13.namespace/scope/main.cpp
@@ -0,0 +1,25 @@
+#include "../../all.hpp"
+
+auto f = []() {
+  std::cout << 1;
+};
+
+int main() {
+  f();
+
+  auto f = []() {
+    std::cout << 2;
+  };
+
+  f();
+
+  {
+    f();
+
+    auto f = []() {
+      std::cout << 3;
+    };
+    f();
+  }
+  f();
+}
