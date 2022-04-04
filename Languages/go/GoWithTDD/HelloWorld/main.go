ef0181aa61125622334e7b7e233ef1012b3bab4d
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-04-05 02:00:56 +0900

    Languages::go - GoWithTDD refactor and test #56

--- Languages/go/GoWithTDD/HelloWorld/main.go
+++ Languages/go/GoWithTDD/HelloWorld/main.go
@@ -1,17 +1,27 @@
 package main
 
-import "fmt"
-
 const englishHelloPrefix = "Hello, "
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
-		return englishHelloPrefix + "World"
-
+		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
 }
 
-func main() {
-	fmt.Println(Hello("World"))
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
 }
