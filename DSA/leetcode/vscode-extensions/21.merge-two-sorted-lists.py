cc0beb3a6f4860160ab127cb8bea1df377fca55c
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-04-14 03:13:05 +0900

    DSA::leetcode 20 merge sort with linkedlist #60

---
+++ DSA/leetcode/vscode-extensions/21.merge-two-sorted-lists.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=21 lang=python3
+#
+# [21] Merge Two Sorted Lists
+#
+from typing import List, Optional
+
+
+class ListNode:
+    # Definition for singly-linked list.
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# @lc code=start
+class Solution:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        temp = result = ListNode()
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                temp.next = ListNode(list1.val)
+                list1 = list1.next
+            else:
+                temp.next = ListNode(list2.val)
+                list2 = list2.next
+            temp = temp.next
+
+        while list1:
+            temp.next = ListNode(list1.val)
+            temp = temp.next
+            list1 = list1.next
+
+        while list2:
+            temp.next = ListNode(list2.val)
+            temp = temp.next
+            list2 = list2.next
+
+        return result.next
+
+
+# @lc code=end
