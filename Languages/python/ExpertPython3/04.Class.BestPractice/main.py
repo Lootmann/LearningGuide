4479e40fc5ea384143a000dd79ef13a698bd0217
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-04-14 14:33:17 +0900

    Languages::python - ExpertPython3 inheritance std, super() #61

--- Languages/python/ExpertPython3/04.Class.BestPractice/main.py
+++ Languages/python/ExpertPython3/04.Class.BestPractice/main.py
@@ -1,3 +1,22 @@
+from collections import UserDict
+
+
+def title(msg: str):
+    def inner(func):
+        def wrapper(*args, **kwgs):
+            l = len(msg) + 8
+            print(">" * l)
+            print(f">>> {msg}")
+            print(">" * l)
+            result = func(*args, **kwgs)
+            print()
+            return result
+
+        return wrapper
+
+    return inner
+
+
 class Vector:
     def __init__(self, x, y) -> None:
         self.x = x
@@ -16,7 +35,8 @@ class Vector:
         return self.x == other.x and self.y == other.y
 
 
-def main():
+@title("vector")
+def vector():
     v = Vector(2, 3)
     print(v)
 
@@ -27,6 +47,56 @@ def main():
     print(v)
 
     print(v == Vector(0, 0))
+
+
+class DistinctError(ValueError):
+    """"""
+
+
+class distinctdict(UserDict):
+    def __setitem__(self, key, value) -> None:
+        if value in self.values():
+            if (key in self and self[key] != value) or key not in self:
+                raise DistinctError("value should be unique :^)")
+        super().__setitem__(key, value)
+
+
+@title("distinct dict")
+def distinctdict_sample():
+    d = distinctdict()
+    d["key"] = "v"
+    d["kagi"] = "atai"
+    d["key"] = "another"
+    d["wow"] = "hey"
+    d["new"] = "v"
+    print(d)
+
+
+class Mama:
+    def says(self):
+        print("Do your assignment")
+
+
+class Sister(Mama):
+    def says(self):
+        # or super().says()
+        super(Sister, self).says()
+        print("and clean your bedroom")
+
+
+@title("Mama")
+def mama():
+    mama = Mama()
+    mama.says()
+
+    sis = Sister()
+    sis.says()
+
+
+def main():
+    vector()
+    distinctdict_sample()
+    mama()
 
 
 if __name__ == "__main__":
