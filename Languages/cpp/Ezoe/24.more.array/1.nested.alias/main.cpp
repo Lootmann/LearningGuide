16369553b6652992b25540d1094da3241b21f824
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-04-14 16:31:32 +0900

    Languages::cpp - Ezoe front, back, fill #34

--- Languages/cpp/Ezoe/24.more.array/1.nested.alias/main.cpp
+++ Languages/cpp/Ezoe/24.more.array/1.nested.alias/main.cpp
@@ -1,5 +1,9 @@
 #include "../../all.hpp"
 
+auto p = [](auto x) {
+  std::cout << x << "\n"s;
+};
+
 template <typename T, std::size_t N>
 struct Array {
   using value_type = T;
@@ -18,19 +22,54 @@ struct Array {
     return storage[i];
   }
 
+  // front()
+  reference front() {
+    return storage[0];
+  }
+
+  const_reference front() const {
+    return storage[0];
+  }
+
+  // back()
+  reference back() {
+    return storage[N - 1];
+  }
+
+  const_reference back() const {
+    return storage[N - 1];
+  }
+
+  void fill(T const &u) {
+    for (std::size_t i = 0; i != N; ++i) {
+      storage[i] = u;
+    }
+  }
+
+  // size
   size_type size() const {
     return N;
   }
 };
+
 template <typename Array>
 void print(Array const &c) {
   for (std::size_t i = 0; i < c.size(); ++i) {
-    std::cout << c[i];
+    std::cout << c[i] << " "s;
   }
+  std::cout << "\n"s;
 }
 
 int main() {
   Array<int, 5> a = {1, 2, 3, 4, 5};
-
   print(a);
+
+  a.fill(9);
+  print(a);
+
+  int f = a.front();
+  int const cf = a.front();
+
+  p(f);
+  p(cf);
 }
