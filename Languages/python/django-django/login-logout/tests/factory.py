bd4d80e54dd9bea0d1318bbb407afc8065640e8c
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-04-25 00:01:44 +0900

    Languages::python - django:login-logout fix typo

--- Languages/python/django-django/login-logout/tests/factory.py
+++ Languages/python/django-django/login-logout/tests/factory.py
@@ -14,5 +14,5 @@ def create_superuser(username: str, email: str, password: str) -> CustomUser:
     return CustomUser.objects.create_superuser(username=username, email=email, password=password)
 
 
-def create_book(bookname: str) -> BookModel:
-    return BookModel.objects.create(name=bookname)
+def create_book(book_name: str) -> BookModel:
+    return BookModel.objects.create(name=book_name)
