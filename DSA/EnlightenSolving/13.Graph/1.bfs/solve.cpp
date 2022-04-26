b01cfcbed06261ac3629591cb3338f2b37d4d441
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-04-26 20:30:19 +0900

    DSA::EnlightenSolving - Graph dfs and bfs, so cool #32

--- DSA/EnlightenSolving/13.Graph/1.bfs/solve.cpp
+++ DSA/EnlightenSolving/13.Graph/1.bfs/solve.cpp
@@ -4,6 +4,80 @@ using namespace std;
 
 using Graph = vector<vector<int>>;
 
+void title(string msg) {
+  cout << ">>> " << msg << "\n"s;
+}
+
+auto trace = [](auto graph, auto n) {
+  title("trace");
+
+  for (int i = 0; i < n; ++i) {
+    cout << i << " {";
+    for (auto cell : graph[i]) {
+      cout << cell << ' ';
+    }
+    cout << "} \n";
+  }
+};
+
+// Depth-First Search
+void dfs(const Graph &G, int start) {
+  title("Depth-First Search");
+
+  int n = (int)G.size();
+  vector<bool> seen(n, false);
+
+  seen[start] = true;
+
+  stack<int> todo;
+  todo.push(start);
+
+  while (!todo.empty()) {
+    int v = todo.top();
+    todo.pop();
+
+    for (int nv : G[v]) {
+      if (!seen[nv]) {
+        seen[nv] = true;
+        todo.push(nv);
+      }
+    }
+  }  // while
+
+  for (std::size_t i = 0; i < seen.size(); ++i) {
+    cout << i << " " << seen[i] << '\n';
+  }
+}
+
+// Breadth-First Search
+void bfs(const Graph &G, int start) {
+  title("Breadth-First Search");
+
+  int n = (int)G.size();
+  vector<bool> seen(n, false);
+
+  seen[start] = true;
+
+  queue<int> todo;
+  todo.push(start);
+
+  while (!todo.empty()) {
+    int vector = todo.front();
+    todo.pop();
+
+    for (int nv : G[vector]) {
+      if (!seen[nv]) {
+        seen[nv] = true;
+        todo.push(nv);
+      }
+    }
+  }  // while
+
+  for (int i = 0; i < n; ++i) {
+    cout << i << " " << seen[i] << '\n';
+  }
+}
+
 int main() {
   FastIO;
 
@@ -15,16 +89,16 @@ int main() {
     int from, to;
     cin >> from >> to;
 
-    // undirected
+    // directed
     G[from].emplace_back(to);
-    G[to].emplace_back(from);
+
+    // undirected
+    // G[to].emplace_back(from);
   }
 
-  for (int i = 0; i < n; ++i) {
-    cout << i << " {";
-    for (auto cell : G[i]) {
-      cout << cell << ' ';
-    }
-    cout << "} \n";
-  }
+  trace(G, n);
+
+  // start from 0
+  bfs(G, 0);
+  dfs(G, 0);
 }
