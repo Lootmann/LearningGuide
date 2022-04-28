da5e0510bfaecb24989c2c411232f72a743fac91
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-04-28 17:20:03 +0900

    DSA::leetcode 88 merge O(n) #60

--- DSA/leetcode/vscode-extensions/88.merge-sorted-array.py
+++ DSA/leetcode/vscode-extensions/88.merge-sorted-array.py
@@ -3,22 +3,33 @@
 #
 # [88] Merge Sorted Array
 #
+from operator import le
 from typing import List
 
 
 # @lc code=start
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, n1: List[int], m: int, n2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        right = 0
+        a, b = m - 1, n - 1
+        tar = m + n - 1
 
-        while n > right:
-            nums1[m + right] = nums2[right]
-            right += 1
+        while a >= 0 and b >= 0:
+            if n1[a] > n2[b]:
+                n1[tar] = n1[a]
+                a -= 1
+            else:
+                n1[tar] = n2[b]
+                b -= 1
+            tar -= 1
 
-        nums1.sort()
+        while b >= 0:
+            n1[tar] = n2[b]
+            tar -= 1
+            b -= 1
+
         return
 
 
