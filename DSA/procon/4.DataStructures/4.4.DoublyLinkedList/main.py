e9f82f1045a10cb0eb36f4d8b042f245d3648c2d
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-05 21:26:23 +0900

    DSA::procon AOJ ALDS1_3_C: DoublyLinkedList refactor #71

--- DSA/procon/4.DataStructures/4.4.DoublyLinkedList/main.py
+++ DSA/procon/4.DataStructures/4.4.DoublyLinkedList/main.py
@@ -2,76 +2,99 @@ import sys
 
 
 class Node:
-    def __init__(self, val):
-        self.val = val
+    next: "Node"
+    prev: "Node"
+
+    def __init__(self, key: str):
+        self.key = key
         self.next = self
         self.prev = self
 
 
 class DoublyLinkedList:
-    def __init__(self):
-        self._dummy = Node(-1)
+    # sentinel, dummy, nil, and so forth.
+    _dummy: Node
 
-    def insert(self, x):
+    def __init__(self):
+        self._dummy = Node("")
+
+    def insert(self, x: str):
         new_node = Node(x)
         new_node.next = self._dummy.next
         self._dummy.next.prev = new_node
         self._dummy.next = new_node
         new_node.prev = self._dummy
 
-    def delete(self, x):
+    def delete(self, x: str) -> None:
+        # find all elements O(n)
         cur = self._dummy.next
-        while (cur is not self._dummy) and cur.val != x:
+        while (cur is not self._dummy) and cur.key != x:
             cur = cur.next
+
+        if cur is self._dummy:
+            return
 
         self._del(cur)
 
-    def delete_first(self):
-        self._del(self._dummy.next)
+    def delete_first(self) -> None:
+        first = self._dummy.next
 
-    def delete_last(self):
-        self._del(self._dummy.prev)
+        if self._dummy is first:
+            return
 
-    def _del(self, node):
+        self._del(first)
+
+    def delete_last(self) -> None:
+        last = self._dummy.prev
+
+        if self._dummy is last:
+            return
+
+        self._del(last)
+
+    def _del(self, node: Node) -> None:
         if node is self._dummy:
             return
 
         node.prev.next = node.next
         node.next.prev = node.prev
 
-    def print(self):
+    def __str__(self) -> str:
         lists = []
         cur = self._dummy.next
 
         while cur is not self._dummy:
-            lists.append(cur.val)
+            lists.append(cur.key)
             cur = cur.next
 
-        sys.stdout.write(" ".join(lists))
-        sys.stdout.write("\n")
+        return " ".join(lists)
 
 
 def main():
     dll = DoublyLinkedList()
 
-    for command in sys.stdin.readlines():
-        if "deleteFirst" in command:
+    # get all inputs
+    _ = sys.stdin.readline()
+
+    for line in sys.stdin.readlines():
+        if "deleteFirst" in line:
             dll.delete_first()
 
-        elif "deleteLast" in command:
+        elif "deleteLast" in line:
             dll.delete_last()
 
-        elif "insert" in command:
-            _, num = command.split()
-            dll.insert(num)
+        elif "insert" in line:
+            _, key = line.split()
+            dll.insert(key)
 
-        elif "delete" in command:
-            _, num = command.split()
-            dll.delete(num)
+        elif "delete" in line:
+            _, key = line.split()
+            dll.delete(key)
+
         else:
             pass
 
-    dll.print()
+    print(dll)
 
 
 if __name__ == "__main__":
