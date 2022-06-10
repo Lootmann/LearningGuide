0e1a8d5aa6393bb09b9fd215cae288d91e4af9fb
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-10 21:36:35 +0900

    exercism: nim two_fer easy

--- DSA/exercism/nim/two-fer/two_fer.nim
+++ DSA/exercism/nim/two-fer/two_fer.nim
@@ -1,8 +1,2 @@
-proc twoFer*(): string =
-  return "One for you, one for me."
-
-proc twoFer*(name: string): string =
-  if name == "Alice":
-    return "One for Alice, one for me."
-  else:
-    return "One for Bob, one for me."
+proc twoFer*(name: string = "you"): string =
+  return "One for " & name & ", one for me."
