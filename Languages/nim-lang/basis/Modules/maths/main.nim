7f0ea994cfd5dad2313e212df8efa32f59f0e875
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-10 19:40:06 +0900

    Languages::nim-lang - import Custom Module

---
+++ Languages/nim-lang/basis/Modules/maths/main.nim
@@ -0,0 +1,16 @@
+import math
+import mymod
+import lib/pretty
+
+let 
+  c = 30.0 # degree
+  cRadians = c.degToRad()
+
+echo cRadians
+echo sin(cRadians).round(2)
+
+
+echo 2^5
+echo pow(2.0, 5)
+echo plus(10, 20)
+print "hello"
---
+++ Languages/nim-lang/basis/Modules/maths/mymod.nim
@@ -0,0 +1,6 @@
+# * an asterisk means this plus can load another nim
+proc plus*(a, b:int): int =
+  return a + b
+
+proc minus(a, b: int) :int =
+  return a - b
