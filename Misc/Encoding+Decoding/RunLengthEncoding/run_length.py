306915c4586d6c3c50f62943b52c70efead1ec7b
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-14 20:08:58 +0900

    Misc::Enc+Decoding - RunLength Decoding

--- Misc/Encoding+Decoding/RunLengthEncoding/run_length.py
+++ Misc/Encoding+Decoding/RunLengthEncoding/run_length.py
@@ -1,3 +1,8 @@
+# run_length.py
+
+import re
+
+
 class RunLength:
     def __init__(self):
         pass
@@ -7,8 +12,7 @@ class RunLength:
         """
         compression using Run Length Algorithm
 
-        e.g.)
-        A -> A
+        e.g.) A -> A
         AB -> AB
         AABB -> A2B2
         AABAABA -> A2BA2BA
@@ -35,4 +39,23 @@ class RunLength:
             result.append(str(cnt))
 
         # O(N) ?
+        return "".join(result)
+
+    @classmethod
+    def decoding(cls, encoded: str) -> str:
+        # ^[0-9][0-9]*
+        # first ch is not [0-9], and rest are all digits.
+        matched = re.findall(r"\D\d*", encoded)
+
+        result = []
+        for match in matched:
+            # one char
+            if len(match) == 1:
+                result.append(match)
+            else:
+                # A1, B90, C9999, ...
+                digit = match[0]
+                rep = int(match[1:])
+                result.append(digit * rep)
+
         return "".join(result)
--- Misc/Encoding+Decoding/RunLengthEncoding/test_run_length.py
+++ Misc/Encoding+Decoding/RunLengthEncoding/test_run_length.py
@@ -1,7 +1,7 @@
 from run_length import RunLength
 
 
-class TestRunoengthEncoding:
+class TestRunLengthEncoding:
     def setup_method(self):
         self.tests = [
             ("A", "A"),
@@ -30,4 +30,36 @@ class TestRunoengthEncoding:
     def test_encoding_long(self):
         for (src, want) in self.tests_long:
             got = RunLength.encoding(src)
+            assert got == want
+
+
+class TestRunLengthDecoding:
+    def setup_method(self):
+        self.tests = [
+            ("A", "A"),
+            ("A2", "AA"),
+            ("ABCD", "ABCD"),
+            ("AB2AB2", "ABBABB"),
+            ("A3B3A3B3", "AAABBBAAABBB"),
+        ]
+
+        self.tests_long = [
+            (
+                "W12BW12B3W24BW14",
+                "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW",
+            ),
+            (
+                "kw5b5w5b5w5b5w5b5w5b5w5b5w5b5w5b5k",
+                "kwwwwwbbbbbwwwwwbbbbbwwwwwbbbbbwwwwwbbbbbwwwwwbbbbbwwwwwbbbbbwwwwwbbbbbwwwwwbbbbbk",
+            ),
+        ]
+
+    def test_decoding(self):
+        for (src, want) in self.tests:
+            got = RunLength.decoding(src)
+            assert got == want
+
+    def test_decoding_long(self):
+        for (src, want) in self.tests_long:
+            got = RunLength.decoding(src)
             assert got == want
