517754ba4265f647d520fc7a0acab29e27fc7ad7
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-15 20:39:15 +0900

    DSA::exercism - str: reverse string #71

---
+++ DSA/exercism/python/reverse-string/reverse_string.py
@@ -0,0 +1,11 @@
+""" Reverse String
+return Reversed String
+"""
+
+
+def reverse(text: str) -> str:
+    """reverse
+    :param str: text
+    :return: str - reversed text
+    """
+    return text[::-1]
