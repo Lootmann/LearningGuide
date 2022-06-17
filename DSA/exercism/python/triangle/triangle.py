a0af18c15016994ffb951cb0778ed0f06117b53d
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-17 19:14:48 +0900

    DSA::exercism - python bool - refactoring #71

--- DSA/exercism/python/triangle/triangle.py
+++ DSA/exercism/python/triangle/triangle.py
@@ -3,31 +3,23 @@ from typing import List
 TriangleSide = List[float]
 
 
-def is_triangle(sides: TriangleSide) -> bool:
-    sides.sort()
+def is_valid_triangle(func):
+    def inner(sides: TriangleSide) -> bool:
+        return sum(sides) > 2 * max(sides) and func(sides)
 
-    if 0 in sides:
-        return False
-
-    if sides[0] + sides[1] < sides[2]:
-        return False
-
-    return True
+    return inner
 
 
+@is_valid_triangle
 def equilateral(sides: TriangleSide) -> bool:
-    if not is_triangle(sides):
-        return False
     return sides[0] == sides[1] and sides[1] == sides[2]
 
 
+@is_valid_triangle
 def isosceles(sides: TriangleSide) -> bool:
-    if not is_triangle(sides):
-        return False
     return sides[0] == sides[1] or sides[1] == sides[2] or sides[2] == sides[0]
 
 
+@is_valid_triangle
 def scalene(sides: TriangleSide) -> bool:
-    if not is_triangle(sides):
-        return False
     return sides[0] != sides[1] and sides[1] != sides[2] and sides[2] != sides[0]
