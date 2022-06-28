9463c30ff621519026e957f4a55138f520f00f85
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-28 10:42:39 +0900

    DSA::exercism - python easy #71

---
+++ DSA/exercism/python/two-fer/two_fer.py
@@ -0,0 +1,13 @@
+""" two_fer.py
+
+Implement two-fer
+"""
+
+
+def two_fer(name: str = "you") -> str:
+    """two_fer
+
+    :param name: str - name
+    :return: str  - two_fer string
+    """
+    return f"One for {name}, one for me."
