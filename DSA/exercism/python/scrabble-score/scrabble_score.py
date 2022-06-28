d45249c650b3e470b9197c8041668c89706a26e6
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-28 13:16:51 +0900

    DSA::exercism - python easy #71

---
+++ DSA/exercism/python/scrabble-score/scrabble_score.py
@@ -0,0 +1,46 @@
+""" scrabble_score.py
+
+Given a word, compute the Scrabble score for that word.
+"""
+
+
+def score(word: str) -> int:
+    """score
+
+    :param word: str
+    :return: int
+    """
+
+    d = {
+        "A": 1,
+        "E": 1,
+        "I": 1,
+        "O": 1,
+        "U": 1,
+        "L": 1,
+        "N": 1,
+        "R": 1,
+        "S": 1,
+        "T": 1,
+        "D": 2,
+        "G": 2,
+        "B": 3,
+        "C": 3,
+        "M": 3,
+        "P": 3,
+        "F": 4,
+        "H": 4,
+        "V": 4,
+        "W": 4,
+        "Y": 4,
+        "K": 5,
+        "J": 8,
+        "X": 8,
+        "Q": 10,
+        "Z": 10,
+    }
+
+    result = []
+    for ch in word.upper():
+        result.append(d.get(ch))
+    return sum(result)
