0fb2647c49044da15f51f6b811de5bc6e84d90b3
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-07-06 14:25:03 +0900

    Languages::python - DjangoForProfessionals - ch.14 SearchList queryset #66

--- Languages/python/DjangoForProfessionals/03.bookstore.project/books/urls.py
+++ Languages/python/DjangoForProfessionals/03.bookstore.project/books/urls.py
@@ -1,11 +1,12 @@
 # books/urls.py
 from django.urls import path
 
-from books.views import BookDetailView, BookListView
+from books.views import BookDetailView, BookListView, BookSearchListView
 
 app_name = "books"
 
 urlpatterns = [
     path("", BookListView.as_view(), name="book_list"),
     path("<uuid:pk>/", BookDetailView.as_view(), name="book_detail"),
+    path("search/", BookSearchListView.as_view(), name="search_results"),
 ]
--- Languages/python/DjangoForProfessionals/03.bookstore.project/books/views.py
+++ Languages/python/DjangoForProfessionals/03.bookstore.project/books/views.py
@@ -1,5 +1,6 @@
 # books/views.py
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import Q
 from django.views import generic
 
 from books.models import Book
@@ -18,3 +19,12 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, generic.Detail
     template_name = "books/book_detail.html"
     login_url = "account_login"
     permission_required = "books.special_status"
+
+
+class BookSearchListView(generic.ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+
+    def get_queryset(self):
+        return Book.objects.filter(Q(title__icontains="beginners") | Q(title__icontains="api"))
