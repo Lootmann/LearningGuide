28428b32194be653ffc8f4d3e39e775bae243e06
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-07-13 10:33:17 +0900

    DSA::procon AOJ ALDS1_1_B custom gcd #71

--- DSA/procon/ALDS1/1_B/main.py
+++ DSA/procon/ALDS1/1_B/main.py
@@ -1,24 +1,12 @@
-def divisors(a: int) -> set:
-    s = set()
-
-    for i in range(1, int(a**0.5) + 1):
-        if a % i == 0:
-            s.add(i)
-            s.add(a // i)
-    return s
+def mygcd(a: int, b: int) -> int:
+    if b == 0:
+        return a
+    return mygcd(b, a % b)
 
 
 def main():
     x, y = map(int, input().split())
-
-    x_set = divisors(x)
-    y_set = divisors(y)
-
-    ans = 1
-    for num in x_set:
-        if num in y_set:
-            ans = max(ans, num)
-    print(ans)
+    print(mygcd(x, y))
 
 
 if __name__ == "__main__":
