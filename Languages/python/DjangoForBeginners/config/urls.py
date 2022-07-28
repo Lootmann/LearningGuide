f360af19b1f48bcacfa83a0cf828efed9df8f83f
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-07-29 05:05:38 +0900

    fixed pages and add tests

--- Languages/python/DjangoForBeginners/config/urls.py
+++ Languages/python/DjangoForBeginners/config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # pages
     path("", include("pages.urls")),
+    path("posts/", include("posts.urls")),
 ]
--- Languages/python/DjangoForBeginners/pages/urls.py
+++ Languages/python/DjangoForBeginners/pages/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from pages.views import HomePageView, AboutPageView
 
+app_name = "pages"
+
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
     path("", HomePageView.as_view(), name="home"),
